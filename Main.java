2f3d12e91f1e6fb8febbf80d2538b0457298449f
Aaron Zhang <aaronbxzhang@gmail.com>
2025-10-31 13:25:25 -0400

    add conditional to terminate program if os is windows

--- Main.java
+++ Main.java
@@ -2,14 +2,18 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
+        if (System.getProperty("os.name").toLowerCase().startsWith("windows")) {
+            System.out.println("This odometer app does not support windows");
+            return;
+        }
 
-        String os = getOS();
-        System.out.println(os);
-        System.out.println();
-    }
+        String inputDevDir = "/dev/input/";
 
-    public static String getOS() {
-        return System.getProperty("os.name");
+        
+
+        // String os = getOS();
+        // System.out.println(os);
+        // System.out.println();
     }
 
 }
