330ca4897cb45d78665b9bb1064766e6239925cc
Aaron Zhang <aaronbxzhang@gmail.com>
2025-11-06 19:55:43 -0500

    check for file readablity and read file

--- Main.java
+++ Main.java
@@ -1,18 +1,50 @@
+
+import java.io.IOException;
+
+import java.util.stream.Stream;
+import java.util.List;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
+
     public static void main(String[] args) {
         if (isWindows()) {
-            System.out.println("This odometer app does not support windows");
+            System.out.println("This odometer app does not support windows.");
             return;
         }
 
-        Path inputDevicesDir = Paths.get("/dev/input/");
-        Path inputDeviceInfo = Paths.get("/proc/bus/input/devies");
+        // Path inputDevicesDir = Paths.get("/dev/input/");
+        Path inputDeviceInfo = Paths.get("/proc/bus/input/devices");
+        // Path inputDeviceInfo = Paths.get("/siudhgoaeruihgoiuhg");
+
+        if (!Files.isReadable(inputDeviceInfo)) {
+            System.out.println(inputDeviceInfo + " file was not found");
+            return;
+        }
+
+        // Stream<String> content = null;
+
+        try {
+            Stream<String> content = Files.lines(inputDeviceInfo);
+            List<String> lines = content.toList();
+        
+            for (int i = 0; i < lines.size(); i++) {
+                System.out.println(lines.get(i));
+
+            }
+
+        } catch (IOException error) {
+            System.out.println("Cannot read file");
+            return;
+
+        }
 
 
+
+        System.out.println("Hello");
 
         // String os = getOS();
         // System.out.println(os);
@@ -20,9 +52,8 @@ public class Main {
     }
 
     public static boolean isWindows() {
-        String os = System.getProperty("os.name").toLowerCase(); 
+        String os = System.getProperty("os.name").toLowerCase();
         return os.startsWith("windows");
     }
-
 
 }
